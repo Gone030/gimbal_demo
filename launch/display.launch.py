6150e538607d5a3de928d5fa3d7e74fb6a791f8e
Gone030 <gjkjw_030@naver.com>
2026-01-18 23:46:41 +0900

    rviz2, GUI 테스트

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -1,23 +1,26 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+import xacro
 
 def generate_launch_description():
-    pkg_share = FindPackageShare('gimbal_mani')
-    xacro_path = PathJoinSubstitution([pkg_share, 'urdf', 'gimbal.xacro'])
+    pkg_share = get_package_share_directory('gimbal_mani')
+    xacro_path = os.path.join(pkg_share, 'urdf', 'gimbal.xacro')
 
-    robot_description = Command([
-        'xacro ', xacro_path
-    ])
+    doc = xacro.process_file(xacro_path)
+    robot_desc = doc.toxml()
 
     robot_state_publisher_node = Node(
         package= 'robot_state_publisher',
         executable= 'robot_state_publisher',
         name= 'robot_state_publisher',
         output= 'screen',
-        parameters=[{'robot_description' : robot_description}]
+        parameters=[{'robot_description' : robot_desc,
+                     'use_sim_time': True
+        }]
     )
 
     joint_state_publisher_gui_node = Node(
